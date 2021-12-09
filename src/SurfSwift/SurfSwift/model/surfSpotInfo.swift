d6504e0056edeb4db779f794c251f1473dac9c1a
LaurieCLu <59483905+LaurieClu@users.noreply.github.com>
2021-12-09 11:06:32 +0100

    cleaning structs

--- src/SurfSwift/SurfSwift/model/surfSpotInfo.swift
+++ src/SurfSwift/SurfSwift/model/surfSpotInfo.swift
@@ -14,31 +14,12 @@ struct Records: Codable{
     enum CodingKeys: String, CodingKey {
             case spots = "records"
     }
-    
-//    private var imageName: String
-//
-//    var image: Image {
-//        Image(imageName)
-//    }
-//
-//    private var coordinates: Coordinates
-//
-//    var locationCoordinate: CLLocationCoordinate2D{
-//        CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
-//    }
-//
-//    struct Coordinates: Hashable, Codable {
-//        var latitude: Double
-//        var longitude: Double
-//    }
 }
-//struct Number:
-//
+
 struct Spot: Codable{
     var fields: Fields
     var id: String
 }
-
 
 struct Fields: Codable{
     var difficultyLevel: Int
@@ -51,39 +32,18 @@ struct Fields: Codable{
             case difficultyLevel = "Difficulty Level"
             case destination = "Destination"
             case photo = "Photos"
-//            case idPhoto = "id"
             case stateCountry = "Destination State/Country"
             case geocode = "Geocode"
         }
-    init(from decoder: Decoder) throws {
-            let values = try decoder.container(keyedBy: CodingKeys.self)
-            difficultyLevel = try values.decode(Int.self, forKey: .difficultyLevel)
-            destination = try values.decode(String.self, forKey: .destination)
-            stateCountry = try values.decode(String.self, forKey: .stateCountry)
-            photo = try values.decode([PhotoData].self, forKey: .photo)
-            geocode = try values.decode(String.self, forKey: .geocode)
-        print(difficultyLevel)
-        print(destination)
-        print(stateCountry)
-        print(geocode)
-//        print(photo)
-    }
 }
-//
+
 struct PhotoData: Codable{
     var url: String
     var idPhoto: String
-//
+
     enum CodingKeys: String, CodingKey {
         case idPhoto = "id"
         case url = "url"
-    }
-
-    init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        idPhoto = try values.decode(String.self, forKey: .idPhoto)
-        url = try values.decode(String.self, forKey: .url)
-        print(url)
     }
 }
 
